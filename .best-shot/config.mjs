ca01962c7d908457f2ceb78b33a20bb51c6f7213
Eric <airkro@qq.com>
2025-01-07 18:56:41 +0800

    refactor: wip

--- .best-shot/config.mjs
+++ .best-shot/config.mjs
@@ -1,3 +1,4 @@
+import { RsdoctorWebpackPlugin } from '@rsdoctor/webpack-plugin';
 import { Json } from 'fs-chain';
 
 new Json(process.cwd())
@@ -16,20 +17,35 @@ export const config = {
     path: 'dist',
     module: true,
   },
-  externals: {
-    '@yarnpkg/lockfile': 'node-commonjs @yarnpkg/lockfile',
-    'eslint-module-utils/resolve':
-      'node-commonjs eslint-module-utils/resolve.js',
-    // 'flat-cache': 'node-commonjs flat-cache',
-    'postcss-sass': 'node-commonjs postcss-sass',
-    'postcss-styl': 'node-commonjs postcss-styl',
-    // 'write-file-atomic': 'node-commonjs write-file-atomic',
-    eslint: 'module eslint',
-    stylelint: 'module stylelint',
-    sugarss: 'node-commonjs sugarss',
-    'stylelint-scss/src/rules/dollar-variable-empty-line-before/index.js':
-      'node-commonjs stylelint-scss/src/rules/dollar-variable-empty-line-before/index.js',
-    'stylelint-scss/src/rules/double-slash-comment-empty-line-before/index.js':
-      'node-commonjs stylelint-scss/src/rules/double-slash-comment-empty-line-before/index.js',
+  externals: [
+    '@eslint-community/eslint-utils',
+    '@stylistic/eslint-plugin-js',
+    '@stylistic/eslint-plugin-jsx',
+    '@stylistic/eslint-plugin-ts',
+    '@typescript-eslint/parser',
+    'eslint-scope',
+    'eslint',
+    'eslint/use-at-your-own-risk',
+    'stylelint',
+    'typescript',
+    'vue-eslint-parser',
+    {
+      '@yarnpkg/lockfile': 'node-commonjs @yarnpkg/lockfile',
+      'postcss-sass': 'node-commonjs postcss-sass',
+      'postcss-styl': 'node-commonjs postcss-styl',
+      sugarss: 'node-commonjs sugarss',
+      'stylelint-scss/src/rules/dollar-variable-empty-line-before/index.js':
+        'node-commonjs stylelint-scss/src/rules/dollar-variable-empty-line-before/index.js',
+      'stylelint-scss/src/rules/double-slash-comment-empty-line-before/index.js':
+        'node-commonjs stylelint-scss/src/rules/double-slash-comment-empty-line-before/index.js',
+    },
+  ],
+  externalsType: 'module-import',
+  chain(chain) {
+    // chain.plugin('rsdoctor').use(RsdoctorWebpackPlugin, [
+    //   {
+    //     port: 5858,
+    //   },
+    // ]);
   },
 };
