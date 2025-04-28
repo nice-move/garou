9696238deed38741f4d3a92207a5fb27e722caf0
Eric Chen <airkro@qq.com>
2025-04-28 22:44:13 +0800

    chore(deps): update

---
+++ test/fixture/temp/index.jsx
@@ -0,0 +1,55 @@
+// @ts-nocheck
+/* This is a comment with no whitespace at the beginning */
+'use strict';
+
+import { foo, z } from 'bar';
+
+import 'll';
+
+import n, { kde } from 'nn';
+import c from 'vvv';
+
+import ddd from './ss';
+
+const k = [];
+
+class A {
+  constructor() {}
+
+  a() {}
+}
+
+function a() {}
+
+function x() {
+  if (xsd) {
+  }
+}
+
+const y = [];
+
+export default {
+  data() {},
+  a: `${5 ** 2}`,
+  kk: '444"',
+};
+
+export { e } from '../..';
+export { g } from '.';
+export { jkjk, xxxx } from 'c';
+export { h } from 'x';
+
+if (ytt) {
+} else {
+}
+
+export function aaaaa() {}
+
+if (xsd) {
+}
+
+const d = (
+  <R a={1} df={true} prop="Hello world">
+    {'Hello world'}
+  </R>
+);
